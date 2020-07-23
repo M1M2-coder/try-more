24c80e71ea1023bd273a9b7919fd73bced90d7ce
M1M2-coder <m1m2.coder@gmail.com>
2020-07-23 22:06:27 +0300

    Successfully Updating Java Version, and fixing JSP errors

---
+++ shoppingbackend/src/main/java/com/m2m/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.m2m.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
